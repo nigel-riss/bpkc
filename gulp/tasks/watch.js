37ad75ff820773419e9e90cc4d593f09f6f74de6
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2017-04-12 17:56:58 +0200

    Folder structure and index.pug markup, gulp workflow

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,33 @@
+const gulp = require('gulp');
+const watch = require('gulp-watch');
+const browserSync = require('browser-sync');
+
+gulp.task('watch', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'dist'
+        }
+    });
+
+    // pug
+    watch('./src/*.pug', function() {
+        gulp.start('pugRender');
+    });
+
+    // html
+    watch('./dist/*.html', function() {
+        browserSync.reload();
+    });
+
+    // styles
+    watch('./src/**/*.scss', function() {
+        gulp.start('cssInject');
+    })
+
+    // scripts
+});
+
+gulp.task('cssInject', ['styles'], function() {
+    return gulp.src('./dist/styles.css')
+        .pipe(browserSync.stream());
+});
