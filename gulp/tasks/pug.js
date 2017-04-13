840cb5aeb3102527313e081512888fe9561ab4ab
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2017-04-14 01:48:32 +0200

    Main page adaptive draft finished

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const pug = require('gulp-pug2');
 
 gulp.task('pugRender', function() {
-    return gulp.src('./src/*.pug')
+    return gulp.src('./src/**/*.pug')
         .pipe(pug())
         .on('error', function(errorInfo) {
             console.log(errorInfo.toString());
